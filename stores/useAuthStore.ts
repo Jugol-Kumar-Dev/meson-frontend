5e85743207ecb46e31bcc5c4668a510e4a569e38
jugol-kumar <jk23717933@gmail.com>
2024-09-14 15:47:20 +0600

    add pinia auth store

---
+++ stores/useAuthStore.ts
@@ -0,0 +1,49 @@
+import {defineStore} from 'pinia'
+import {useApiFetch} from "~/composables/useApiFetch";
+import {useTokenStore} from "~/stores/useTokenStore";
+
+export default defineStore('auth', ()=>{
+    async function fetchUser(){
+        return useApiFetch("/user", {
+            method: "GET"
+        });
+    }
+
+
+    async function login(credential: any){
+        await $fetch(useRuntimeConfig().public.baseUrl+ '/sanctum/csrf-cookie')
+        return useApiFetch("/login", {
+            method: "POST",
+            body: credential,
+            lazy: true
+        });
+    }
+
+    async function signup(signupData: any){
+        return useApiFetch("/register", {
+            method: "POST",
+            body: signupData,
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            }
+        });
+    }
+
+    // @ts-ignore
+    async function logout(){
+        const {data, error} = await useApiFetch("logout",{method:"POST"})
+        if(data.value) {
+            // @ts-ignore
+            useTokenStore().removeToken()
+            return navigateTo('/login')
+        }
+        if(error.value) {
+            // @ts-ignore
+            useTokenStore().removeToken()
+            return navigateTo('/login')
+        }
+    }
+
+    return { login,signup, logout, fetchUser}
+
+})
