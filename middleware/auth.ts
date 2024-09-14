ce5d7c73e08ce57a8a40dc9d8dc397b55c14e098
jugol-kumar <jk23717933@gmail.com>
2024-09-14 15:48:53 +0600

    configure auth middleware for this project

---
+++ middleware/auth.ts
@@ -0,0 +1,7 @@
+
+
+export default defineNuxtRouteMiddleware((to, from) => {
+    // @ts-ignore
+    const {isLogin} = useTokenStore();
+    if (!isLogin) return navigateTo(`/login?redirect=${to.path}`)
+})
