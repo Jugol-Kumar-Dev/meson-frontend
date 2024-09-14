5cb213d4408ccc1836a5d9b8492f4de9e54c0b3c
jugol-kumar <jk23717933@gmail.com>
2024-09-14 15:53:50 +0600

    add app url and some api prefix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon"]
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    css:["~/assets/css/main.css"],
+    modules: [
+        "@nuxtjs/tailwindcss",
+        "@nuxt/icon",
+        "@pinia/nuxt",
+        "pinia-plugin-persistedstate/nuxt"
+    ],
+    runtimeConfig: {
+        public: {
+            baseUrl: import.meta.env.BASE_URL,
+            appUrl: import.meta.env.APP_URL,
+            frontendPrefix: import.meta.env.FRONTEND_API_PREFIX,
+            studentApiPrefix: import.meta.env.STUDNET_API_REFIX,
+            frontendAppUrl: import.meta.env.BASE_URL + import.meta.env.FRONTEND_API_PREFIX,
+            studentAppUrl: import.meta.env.BASE_URL + import.meta.env.STUDENT_API_PREFIX,
+        }
+    }
 })
