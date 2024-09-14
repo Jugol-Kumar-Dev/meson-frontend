2e74c71587d2935db7f00850ff5ec4c4c3c64546
jugol-kumar <jk23717933@gmail.com>
2024-09-14 15:55:08 +0600

    add courses file

---
+++ pages/Courses/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <Container>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet aspernatur dignissimos dolorem, doloremque et eveniet ex fugit libero maiores maxime minus natus nemo obcaecati provident quam repellat sapiente. Molestias.
+    </Container>
+  </section>
+</template>
+<script setup lang="ts">
+</script>
