d2ce3ae462f8ca94658cc5730b3aa877d7bd1618
jugol-kumar <jk23717933@gmail.com>
2024-09-14 15:51:05 +0600

    configure default primary and secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import colors from "tailwindcss/colors.js";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content:  [
@@ -11,8 +13,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#4D28AC'
-        
+        primary: {
+          '50': '#f4f4fe',
+          '100': '#eceafd',
+          '200': '#dad8fc',
+          '300': '#bfb9f9',
+          '400': '#9f91f4',
+          '500': '#7f64ee',
+          '600': '#6c43e4',
+          '700': '#5e31d0',
+          '800': '#4d28ac',
+          '900': '#41238f',
+          '950': '#271461',
+        },
+        secondary:colors.slate
       }
     },
   },
