726f7ed76cb54cdd4e09d0ef350383b77ad1318a
jugol-kumar <jk23717933@gmail.com>
2024-09-14 15:49:53 +0600

    setup login function

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,37 +1,87 @@
-<script setup >
-import {ref, onMounted} from "vue";
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import { FreeMode, Navigation, Thumbs,Pagination,Autoplay } from 'swiper/modules';
+<script setup>
+import {Swiper, SwiperSlide} from 'swiper/vue';
+import {FreeMode, Navigation, Thumbs, Autoplay} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
-const modules  = [FreeMode, Navigation, Thumbs,Autoplay];
+
+const modules = [FreeMode, Navigation, Thumbs, Autoplay];
+const route = useRoute()
+
+definePageMeta({
+  middleware: ['guest']
+})
+
+const { setToken, setAuthUser } = useTokenStore();
+const form = ref()
+const state = reactive({
+  email: 'vendor@vendor.com',//undefined,
+  password: '12345678',//undefined,
+  remember: false
+})
+
+
+const {login, fetchUser} = useAuthStore();
+const pending = ref(false)
+const errors = ref({})
+
+
+const onSubmit = async () => {
+  pending.value = true;
+  const {data, error} = await login(state)
+  errors.value = error?.value?.data
+  if(error?.value){
+    alert(error)
+  }
+  if (!error.value){
+    setToken(data.value)
+    const userData = await fetchUser();
+    setAuthUser(userData?.data?.value)
+    alert('login successfully done...')
+    if(userData?.data?.value){
+      return navigateTo(route?.query?.redirect ?? '/student/dashboard')
+    }
+  }
+  pending.value = false;
+}
 
 </script>
 
 
 <template>
-    <Container>
-        <div class="flex flex-col lg:flex-row items-center justify-center h-screen">
-            <div class="w-full lg:w-1/2 pt-20">
-                <div class="max-w-sm mx-auto px-6 lg:px-0">
-                    <h2 class="text-lg lg:text-xl font-bold text-gray-600 pb-5">Proceed With Mobile Number/Email</h2>
-                        <input type="text" id="table-search-users"
-                            class="block border border-primary w-80 p-3 ps-2 text-sm rounded text-primary bg-white placeholder-slate-500 focus:ring-0 focus:border-primary focus:outline-0"
-                            placeholder="Phone Number / Email ">
-                          
-                        <button class="flex items-center justify-center gap-3 my-5 w-80 rounded bg-primary hover:bg-purple-900 text-white px-4 py-2 text-lg font-semibold">
-                            Submit
-                        </button>
-                       
-                </div>
-            </div>
-            <div class="w-full lg:w-1/2">
-                <div class="max-w-sm mx-auto px-8 lg:px-0">
-                    <h2 class="text-lg lg:text-xl font-bold text-gray-600 pb-5 text-center">Maintain Study Routine by Attending Daily Live Classes</h2>
-                    <Swiper
-                            :breakpoints="{
+  <Container>
+    <div class="flex flex-col lg:flex-row items-center justify-center h-screen">
+      <div class="w-full lg:w-1/2 pt-20">
+        <form @submit.prevent="onSubmit" class="max-w-sm mx-auto px-6 lg:px-0">
+          <h2 class="text-lg lg:text-xl font-bold text-gray-600 pb-5">Proceed With Mobile Number/Email</h2>
+          <div class="flex flex-col gap-4">
+            <input type="text" id="table-search-users"
+                   v-model="state.email"
+                   class="block border border-primary w-80 p-3 ps-2 text-sm rounded text-primary-500 bg-white placeholder-slate-500 focus:ring-0 focus:border-primary focus:outline-0"
+                   placeholder="Email Address">
+            <span v-if="errors?.errors?.email" class="text-red-500">{{ errors?.errors?.email[0] }}</span>
+            <input type="password"
+                   v-model="state.password"
+                   id="table-search-users"
+                   class="block border border-primary w-80 p-3 ps-2 text-sm rounded text-primary-500 bg-white placeholder-slate-500 focus:ring-0 focus:border-primary focus:outline-0"
+                   placeholder="Password">
+          </div>
+          <button
+              class="flex items-center justify-center gap-3 my-5 w-80 rounded bg-primary-500 hover:bg-purple-900 text-white px-4 py-2 text-lg font-semibold">
+            Submit
+          </button>
+
+        </form>
+      </div>
+
+
+      <div class="w-full lg:w-1/2">
+        <div class="max-w-sm mx-auto px-8 lg:px-0">
+          <h2 class="text-lg lg:text-xl font-bold text-gray-600 pb-5 text-center">Maintain Study Routine by Attending
+            Daily Live Classes</h2>
+          <Swiper
+              :breakpoints="{
                             '375': {
                                 slidesPerView: 1,
                                 spaceBetween: 10,
@@ -45,38 +95,42 @@ const modules  = [FreeMode, Navigation, Thumbs,Autoplay];
                                 spaceBetween: 10,
                             },
                         }"
-                            :centeredSlides="true"
-                            :speed= "500"
-                            :freeMode="true"
-                            :freeModeMomentum="false"
-                            :freeModeMomentumBounce="false"
-                            :grabCursor="true"
-                            :autoplay= "{
+              :centeredSlides="true"
+              :speed="500"
+              :freeMode="true"
+              :freeModeMomentum="false"
+              :freeModeMomentumBounce="false"
+              :grabCursor="true"
+              :autoplay="{
                             delay:3000,
                             disableOnInteraction: true,
                         }"
-                            :loop="true"
-                            :allowTouchMove="true"
-                            :disableOnInteraction="true"
-                            :modules="modules"
-                            class="mySwiper"
-                        >
-                        <SwiperSlide>
-                            <img class="w-full h-72 object-cover p-5 rounded-xl" src="https://app.10minuteschool.com/assets/login/routine3.svg" alt="" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img class="w-full object-cover h-72 p-5 rounded-xl" src="https://app.10minuteschool.com/assets/login/routine.svg" alt="" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img class="w-full object-cover h-72 p-5 rounded-xl" src="https://app.10minuteschool.com/assets/login/routine1.svg" alt="" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img class="w-full h-72  p-5 rounded-xl" src="https://app.10minuteschool.com/assets/login/routine2.svg" alt="" />
-                        </SwiperSlide>
-                        
-                    </Swiper>   
-                </div>
-            </div>
+              :loop="true"
+              :allowTouchMove="true"
+              :disableOnInteraction="true"
+              :modules="modules"
+              class="mySwiper"
+          >
+            <SwiperSlide>
+              <img class="w-full h-72 object-cover p-5 rounded-xl"
+                   src="https://app.10minuteschool.com/assets/login/routine3.svg" alt=""/>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img class="w-full object-cover h-72 p-5 rounded-xl"
+                   src="https://app.10minuteschool.com/assets/login/routine.svg" alt=""/>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img class="w-full object-cover h-72 p-5 rounded-xl"
+                   src="https://app.10minuteschool.com/assets/login/routine1.svg" alt=""/>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img class="w-full h-72  p-5 rounded-xl" src="https://app.10minuteschool.com/assets/login/routine2.svg"
+                   alt=""/>
+            </SwiperSlide>
+
+          </Swiper>
         </div>
-    </Container>
+      </div>
+    </div>
+  </Container>
 </template>
