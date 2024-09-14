335fd965845a25d671cf09835387c2d1873b85ae
jugol-kumar <jk23717933@gmail.com>
2024-09-14 15:50:10 +0600

    setup checkout form and show course data

--- pages/admission/index.vue
+++ pages/admission/index.vue
@@ -1,114 +1,95 @@
+<script setup lang="ts">
+
+definePageMeta({
+  middleware:['auth']
+})
+
+
+
+
+const route = useRoute();
+const {
+  data: course,
+  status,
+  refresh
+} = await useLazyAsyncData(`single-course-${route.query?.course}`, () => $fetch(`/get-single-course/${route.query?.course}`, {
+  baseURL: useRuntimeConfig().public?.frontendAppUrl,
+  headers: {
+    accept: "application/json",
+  },
+}))
+
+console.log(route.query)
+
+</script>
+
 <template>
-    <section class="relative bg-[url('@/assets/images/banner-3.png')] bg-no-repeat bg-cover bg-top h-auto">
-        <!-- <span class="absolute top-0 left-0 bottom-0 right-0 w-full h-full bg-black/65"></span> -->
-        <Container>
-            <div class="lg:max-w-2xl">
-                <h2 class="text-xl lg:text-4xl text-center lg:text-left font-bold text-white py-10 tracking-wider">Dhaka University A Unit Admission Course - 2024</h2>
-                <p class="text-sm lg:text-base text-gray-300 pb-3 px-3">
-                   Every year lakhs of students pass the HSC examination but the number of seats in public universities is limited. These lakhs of students struggle to grab a seat for themselves in the admission tests. And for science students, this fight is more difficult. This is why to help the students of science background.
-                </p>
-                <p class="text-sm lg:text-base text-gray-300 pb-8 px-3">
-                    This course will make the aspirant's admission journey easy and organized through live classes, lecture sheets, doubt-solving classes, and daily, weekly, and monthly exams. Renowned teachers of the country are present in this course for your guidance and mentorship.
-                </p>
+
+
+  <section class="relative bg-[url('@/assets/images/banner-3.png')] bg-no-repeat bg-cover bg-top min-h-[50vh] h-auto">
+    <!-- <span class="absolute top-0 left-0 bottom-0 right-0 w-full h-full bg-black/65"></span> -->
+    <Container>
+      <div class="lg:max-w-2xl">
+        <h2 class="text-xl lg:text-4xl text-center capitalize lg:text-left font-bold text-white py-10 tracking-wider">
+          Make Admission This Course
+        </h2>
+        <p class="text-sm lg:text-base text-gray-300 pb-3 px-3">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aut autem dignissimos dolore dolorem
+          doloremque dolores dolorum ducimus,
+        </p>
+      </div>
+    </Container>
+  </section>
+  <section class="my-20">
+    <Container>
+      <div class="grid grid-cols-12 gap-10">
+        <div class="col-span-8">
+          <div>
+            <h2 class="px-5 py-2 bg-primary-500 text-white font-bold">Where Are you From?</h2>
+            <div class="mt-10 flex flex-col gap-5">
+              <div class="flex items-center gap-5">
+                <input type="text" class="px-2 w-full rounded-none py-3 border focus:outline-none focus:border-primary" placeholder="Post Office"/>
+                <input type="text" class="px-2 w-full rounded-none py-3 border focus:outline-none focus:border-primary" placeholder="Thana / Sub-District"/>
+                <input type="text" class="px-2 w-full rounded-none py-3 border focus:outline-none focus:border-primary" placeholder="District"/>
+              </div>
+              <div>
+                <input type="text" class="px-2 w-full rounded-none py-3 border focus:outline-none focus:border-primary" placeholder="Full Address"/>
+              </div>
             </div>
-        </Container>
-    </section>
+          </div>
+        </div>
+        <div class="col-span-4">
+          <div class="bg-white lg:-mt-[300px] relative z-40">
+            <img class="p-1 w-full" :src="course?.course?.cover_url" alt="">
+            <div class="flex flex-col gap-5 mb-10 px-5">
+              <div class="flex justify-between items-center">
+                <p class="font-bold text-lg">Course Price: </p>
+                <p class="text-2xl font-semibold p-3">৳ {{ course?.course?.price }}</p>
+              </div>
 
-    <section>
-        <Container>
-            <div>
-              
-                <div class="flex flex-wrap">
-                    <div class="w-full lg:w-2/3"> 
-                         <h2 class="text-xl lg:text-4xl font-bold text-center lg:text-left text-gray-600 py-10 tracking-wider">Course Instructors</h2>
-                        <div class="flex flex-wrap">
-                            <div class="w-1/2 py-3" v-for="item in 6">
-                                <div class="flex flex-col lg:flex-row items-center gap-3">
-                                    <img class="border-2 p-0.5 lg:p-1 w-16 lg:w-32 h-16 lg:h-32 border-primary rounded-full" src="https://lms10.s3.ap-southeast-1.amazonaws.com/crm/square_images/2023-10-03T13:16:42.241Z.png?w=73&h=73" alt="">
-                                    <div class="text-sm lg:text-base text-center lg:text-left font-medium">
-                                        <p>Tanmay Dhar</p>
-                                        <p>DU, CU (8+ Years Exp)</p>
-                                    </div>
-                                </div>
-                            </div>     
-                        </div>
+              <div class="flex justify-between mb-5">
+                <input type="text" class="px-2 w-full rounded-none py-3 border focus:outline-none focus:border-primary" placeholder="enter promo"/>
+                <button class="bg-primary-500 w-[30%] text-white px-2 py-3">Try Now</button>
+              </div>
 
-                        <h2 class="text-xl lg:text-2xl font-bold text-center lg:text-left px-5 lg:px-0 text-gray-600 py-10 tracking-wide">What You Will Learn By Doing The Course</h2>
-                        <div>
-                            <ul class="flex flex-wrap gap-3 lg:gap-7 px-5 lg:px-0">
-                                <li class="flex gap-1">
-                                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" aria-hidden="true" class="mr-1 mt-[2px]" height="20" width="20" xmlns="http://www.w3.org/2000/svg" style="color: rgb(98, 148, 248);"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                                    <p class="max-w-xs text-sm lg:text-base">Basics of 6 subjects as well as shortcuts on various topics</p>
-                                </li>
-                                <li class="flex gap-1 ">
-                                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" aria-hidden="true" class="mr-1 mt-[2px]" height="20" width="20" xmlns="http://www.w3.org/2000/svg" style="color: rgb(98, 148, 248);"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                                    <p class="max-w-xs text-sm lg:text-base">Final revision of each subject before the examination</p>
-                                </li>
-                                <li class="flex gap-1">
-                                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" aria-hidden="true" class="mr-1 mt-[2px]" height="20" width="20" xmlns="http://www.w3.org/2000/svg" style="color: rgb(98, 148, 248);"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                                    <p class="max-w-xs text-sm lg:text-base">Guidance and mentorship from expert teachers</p>
-                                </li>
-                                <li class="flex gap-1 text-sm lg:text-base">
-                                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" aria-hidden="true" class="mr-1 mt-[2px]" height="20" width="20" xmlns="http://www.w3.org/2000/svg" style="color: rgb(98, 148, 248);"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                                    <p class="max-w-xs text-sm lg:text-base">Get to know the time management techniques</p>
-                                </li>
-                            </ul>
-                        </div>   
-                    </div>
+              <NuxtLink :to="`/admission?course=${course?.course?.id}`"
+                        class="flex items-center justify-center gap-3 w-full rounded bg-primary-500 hover:bg-purple-900 text-white px-4 py-2 text-lg font-semibold">
+                Payment Now
+              </NuxtLink>
 
-                    <div class="w-ftll lg:w-1/3 px-2 order-first lg:order-none pt-8">
-                        <div class="bg-white lg:-mt-[300px] relative z-40 ">
-                            <img class="p-1 w-full" src="https://cdn.10minuteschool.com/images/catalog/media/CBnWtpdTuBU-HD_1723713200539.jpg?w=867&h=480" alt="">
-                            <p class="text-2xl font-semibold p-3">৳ 2750</p>
-                            <div class="mx-5">
-                                <button class="flex items-center justify-center gap-3 w-full rounded bg-primary hover:bg-purple-900 text-white px-4 py-2 text-lg font-semibold">
-                                    Enroll Now
-                                </button>
-                            </div>
-
-                            <h2 class="text-xl font-semibold p-3 pt-9">What is in this course</h2>
-                            <ul class="p-3 leading-loose text-gray-600 pl-6">
-                                <li class="flex items-center gap-5">
-                                    <span><Icon name="material-symbols:menu-book-outline-rounded" class="text-lg" /></span>
-                                    <p>6 Subjects</p>
-                                </li>
-                                <li class="flex items-center gap-5">
-                                    <span><Icon name="pajamas:live-stream" class="text-lg" /></span>
-                                    <p>120 Live Classes</p>
-                                </li>
-                                <li class="flex items-center gap-5">
-                                    <span><Icon name="ph:slideshow-fill" class="text-lg" /></span>
-                                    <p>Lecture Slide 120</p>
-                                </li>
-                                <li class="flex items-center gap-5">
-                                    <span><Icon name="mdi:playlist-play" class="text-lg" /></span>
-                                    <p>Daily Exam 120</p>
-                                </li>
-                                <li class="flex items-center gap-5">
-                                    <span><Icon name="hugeicons:task-daily-02" class="text-lg" /></span>
-                                    <p>Weekly Exam 19</p>
-                                </li>
-                                <li class="flex items-center gap-5">
-                                    <span><Icon name="mdi:animation-play" class="text-lg" /></span>
-                                    <p>Model Test 15</p>
-                                </li>
-                                <li class="flex items-center gap-5">
-                                    <span><Icon name="mdi:approximately-equal-box" class="text-lg" /></span>
-                                    <p>Zoom Doubt Solving Class</p>
-                                </li>
-                            </ul>
-
-                            <div class="text-sm flex justify-between py-5 px-3">
-                                <p class="text-gray-400">For details about the course</p>
-                                <p class="flex items-center gap-1 text-primary font-semibold">
-                                    <span class=""><Icon name="material-symbols:call-outline" class="" /></span>
-                                    Call (16910)
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
-        </Container>
-    </section>
+
+            <div class="text-sm flex justify-between py-5 px-3">
+              <p class="text-gray-400">For details about the course</p>
+              <p class="flex items-center gap-1 text-primary-500 font-semibold">
+                <span class=""><Icon name="material-symbols:call-outline" class=""/></span>
+                Call (16910)
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  </section>
+
 </template>
